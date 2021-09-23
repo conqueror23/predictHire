b6d3be03d9fdcde62df4cefec2be7b575677e862
bruce <bruce.wang@zerologix.com>
2021-09-23 15:14:32 +1000

    direct to main api/graphql page

--- nextApp/pages/index.tsx
+++ nextApp/pages/index.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
+import {useRouter} from 'next/router'
 // can be used potentially to sent login info through graphql to restful apis
-export default function index() {
+export default function Index() {
+  const router = useRouter()
+  React.useEffect(()=>{
+    router.push('/api/graphql')
+  })
   return (
     <div>
       <h1>simple graphql bff program</h1>
